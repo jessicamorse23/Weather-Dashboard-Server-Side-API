062e68e4e186330de6c61d5c98c30c75bc96695e
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-09-12 22:05:07 -0400

    added to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,28 @@
-const api = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cee990f46fbf4ad59deda35558a00e4c"; 
+function search(e) {
+  e.preventDefault();
+  var APIKey = "cee990f46fbf4ad59deda35558a00e4c";
+  var textBoxCity = document.getElementById("text-box-city").value;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + textBoxCity + "&appid=" + APIKey;
 
-window.addEventListener("load", () => {
-    let long; 
-    let lat; 
-    // accessing geolocation of user
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            // store latitute and longitude in variables
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            const base = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric";
-            console.log(base); 
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      displayCurrentWeather(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-            fetch(base)
-            .then(response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const {description, icon} = data.weather[0];
-                const {temp} =data.main;
-                const {humid} = data.humid;
-                const {wind} = data.wind;
+function displayCurrentWeather(info) {
+console.log(info) 
+var card = document.createElement("div")
+card.append(info.main.temp)
+var firstCard = document.getElementById("first-card")
+firstCard.append(card)
+}
+var submitBtn = document.getElementById("submit-button");
 
-                const iconUrl = "`http://openweathermap.org/img/wn/${icon}@2x.png`;"
-                const fahrenheit = (temp * 9) / 5 + 32;
-
-                const sunriseGMT = new Date(sunrise * 1000);
-          const sunsetGMT = new Date(sunset * 1000);
-
-            });
-        };
-    
+submitBtn.addEventListener("click", search);
