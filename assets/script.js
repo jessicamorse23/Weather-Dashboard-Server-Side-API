87a532d4777d8f0d3091ed69d38ed9490d3a0a99
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-27 11:00:55 -0400

    button working and url working - rewriting js

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,56 @@
-let apiKey = 'ab8d7152432db2327ddc6c74cb809530';
+var apiKey = 'ab8d7152432db2327ddc6c74cb809530';
+var openWeatherMap = "http://api.openweathermap.org/data/2.5";
+var currentForecast = "weather?"
 let temp = document.querySelector('#temp');
 let humidity = document.querySelector('#humidity');
 let wind = document.querySelector('#wind');
-let currentWeather = document.querySelector('#current-weather');
-let currentIcon = document.querySelector('#current-icon');
+// let currentWeather = document.querySelector('#current-weather');
+// let currentIcon = document.querySelector('#current-icon');
 // let searchedCities = JSON.parse(localStorage.getItem('city'));
 var citySearchButton = document.querySelector('#search');
 // let citySearch = [];
-let current = moment().format('MM/DD/YYYY');
+
 
 function getWeather() {
   // storeWeather();
-  console.log('hello');
-  // let city = document.getElementById('search-city').value;
-  // let apiCity =
-  //   'https://api.openweathermap.org/data/2.5/weather?q=' +
-  //   city +
-  //   '&units=imperial&appid=' +
-  //   apiKey;
+  // console.log('hello');
+  let city = document.getElementById('search').value;
+  let apiCity =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    city +
+    '&units=imperial&appid=' +
+    apiKey;
   // let lat;
   // let lon;
-  // console.log(apiCity);
+  console.log(apiCity);
 
-  // currentWeather.textContent = '';
-  // currentIcon.textContent = '';
-  // temp.textContent = '';
-  // wind.textContent = '';
-  // humidity.textContent = '';
+  currentWeather.textContent = '';
+  currentIcon.textContent = '';
+  temp.textContent = '';
+  wind.textContent = '';
+  humidity.textContent = '';
 
-  // fetch(apiCity)
-  //   .then((response) => response.json())
-  //   .then(function (data) {
-  //     lat = data.coord.lat;
-  //     long = data.coord.lon;
-  //     let currentWeatherApi =
-  //       'https://api.openweathermap.org/data/2.5/forecast?lat=' +
-  //       lat +
-  //       '&lon=' +
-  //       lon +
-  //       '&units=imperial&appid=' +
-  //       apiKey;
+  fetch(apiCity)
+    .then((response) => response.json())
+    .then(function (data) {
+      lat = data.coord.lat;
+      long = data.coord.lon;
+      let currentWeatherApi =
+        'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+        lat +
+        '&lon=' +
+        lon +
+        '&units=imperial&appid=' +
+        apiKey;
 
-  // fetch(currentWeatherApi)
-  //   .then((responseApiCity) => responseApiCity.json())
-  //   .then(function (cityData) {
-  //     console.log(cityData);
-  //     postData(data, cityData);
-  //     return cityData;
-  //   });
-  // });
+  fetch(currentWeatherApi)
+    .then((responseApiCity) => responseApiCity.json())
+    .then(function (cityData) {
+      console.log(cityData);
+      postData(data, cityData);
+      return cityData;
+    });
+  });
 }
 
 citySearchButton.addEventListener('click', getWeather);
