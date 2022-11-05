87cfae71644a35f9ef8f493efef5a0efc558cdbc
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-05 16:17:27 -0400

    click event works

--- assets/script.js
+++ assets/script.js
@@ -12,88 +12,105 @@ var citySearchButton = $('#search');
 
 // let citySearch = [];
 
+citySearchButton.on("click", function (event) {
+  event.preventDefault();
+  // .val gets the value from the textbox - assign val to var
+  var citySearches = textBoxCity.val();
+  locationWeather(citySearches);
 
-function getWeather() {
-  // storeWeather();
-  // console.log('hello');
-  let city =  textBoxCity.val();
-  let apiCity =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    city +
-    '&units=imperial&appid=' +
-    apiKey;
-  // let lat;
-  // let lon;
-  console.log(apiCity);
-  getWeatherNow(apiCity);
-}
+  var location = {
+    location: citySearches
+  }
+// stringify with JSON - set in local storage
+  var saveLocation = JSON.parse(localStorage.getItem(saveLocation)) || [];
 
-function getWeatherNow(apiCity) {
-  fetch(apiCity)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    cityLocation.text(`${data.name}`);
-    temp.text(
-      `${Math.trunc(data.main.temp)} ${String.fromCharCode(176)} °F`
-    );
-    wind.text(`Wind: ${data.wind.speed} MPH`);
-    humidity.text(`Humidity: ${data.main.humidity} %`)
-    let lat = data.coord.lat;
-    let lon = data.coord.lon;
-    currentForecast(lat, lon);
+  localStorage.setItem("saveLocation", JSON.stringify(saveLocation));
 
-  });
-}
+  renderLocalStorage();
+})
 
-function forecast(lat, lon) {
-  let currentForecastApi = 
-  'https://api.openweathermap.org/data/2.5/forecast?lat=' +
-    lat +
-   '&lon=' +
-    lon +
-'&units=imperial&appid=' +
-apiKey;
 
-fiveDayForecast(currentForecastApi);
-}
+// function getWeather() {
+//   // storeWeather();
+//   // console.log('hello');
+//   let city =  textBoxCity.val();
+//   let apiCity =
+//     'https://api.openweathermap.org/data/2.5/weather?q=' +
+//     city +
+//     '&units=imperial&appid=' +
+//     apiKey;
+//   // let lat;
+//   // let lon;
+//   console.log(apiCity);
+//   getWeatherNow(apiCity);
+// }
 
-function fiveDayForecast(currentForecastApi) {
-  fetch(currentForecastApi)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data){
-    console.log(data)
-    let x=0;
-    for (let i =3; i < 36; i += 8) {
-      let time = data.list[i].dt_txt.split(" ");
+// function getWeatherNow(apiCity) {
+//   fetch(apiCity)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     cityLocation.text(`${data.name}`);
+//     temp.text(
+//       `${Math.trunc(data.main.temp)} ${String.fromCharCode(176)} °F`
+//     );
+//     wind.text(`Wind: ${data.wind.speed} MPH`);
+//     humidity.text(`Humidity: ${data.main.humidity} %`)
+//     let lat = data.coord.lat;
+//     let lon = data.coord.lon;
+//     currentForecast(lat, lon);
 
-      document.querySelector(`#day${x}`).children[0].textContent =
-      time[0];
-      document.querySelector(
-        `#day-${x}`
-      ).children[1].textContent = `Temp: ${Math.trunc(
-        data.list[i].main.temp
-      )} ${String.fromCharCode(176)} °F`;
-      document.querySelector(
-        `day-${x}`
-      ).children[2].textContent = `Wind: ${data.list[i].wind.speed} MPH`;
-      document.querySelector(
-        `day-${x}`
-      ).children[3].textContent = `Humidity: ${data.list[i].main.humidity} %`;
-      x++; 
-    }
-  });
-}
-citySearchForm.on("submit", function (e) {
-  e.preventDefault();
-  getWeatherNow();
-  textBoxCity.val("");
+//   });
+// }
 
-});
+// function forecast(lat, lon) {
+//   let currentForecastApi = 
+//   'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+//     lat +
+//    '&lon=' +
+//     lon +
+// '&units=imperial&appid=' +
+// apiKey;
+
+// fiveDayForecast(currentForecastApi);
+// }
+
+// function fiveDayForecast(currentForecastApi) {
+//   fetch(currentForecastApi)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data){
+//     console.log(data)
+//     let x=0;
+//     for (let i =3; i < 36; i += 8) {
+//       let time = data.list[i].dt_txt.split(" ");
+
+//       document.querySelector(`#day${x}`).children[0].textContent =
+//       time[0];
+//       document.querySelector(
+//         `#day-${x}`
+//       ).children[1].textContent = `Temp: ${Math.trunc(
+//         data.list[i].main.temp
+//       )} ${String.fromCharCode(176)} °F`;
+//       document.querySelector(
+//         `day-${x}`
+//       ).children[2].textContent = `Wind: ${data.list[i].wind.speed} MPH`;
+//       document.querySelector(
+//         `day-${x}`
+//       ).children[3].textContent = `Humidity: ${data.list[i].main.humidity} %`;
+//       x++; 
+//     }
+//   });
+// }
+// citySearchForm.on("submit", function (e) {
+//   e.preventDefault();
+//   getWeatherNow();
+//   textBoxCity.val("");
+
+// });
 
 
 
