16f3cc979e3538878facbe1cde40c92fb04cc8e7
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-09 07:44:20 -0500

    main weather is rendering - looks like trash

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,8 @@ citySearchButton.on("click", function (event) {
 // stringify with JSON - set in local storage
   var saveLocation = JSON.parse(localStorage.getItem(saveLocation)) || [];
 
+  saveLocation.unshift(location);
+
   localStorage.setItem("saveLocation", JSON.stringify(saveLocation));
 
   renderLocalStorage();
@@ -31,8 +33,8 @@ var locationWeather = function (apiSearch) {
   .then(function (response) {
     if (response.ok) {
       response.json()
-      .then(function (locationWeatherData) {
-        displayWeather(locationWeatherData);
+      .then(function (apiSearchData) {
+        displayWeather(apiSearchData);
       })
     }
   })
@@ -41,21 +43,21 @@ var locationWeather = function (apiSearch) {
   .then(function (response) {
     if (response.ok) {
       response.json()
-      .then(function (locationWeatherData) {
-        displayWeather(locationWeatherData);
+      .then(function (apiSearchData) {
+        displayWeather(apiSearchData);
       })
     }
   })
 }
 
 // display current weather
-var displayWeather = function (apiSearch) {
+var displayWeather = function (apiSearchData) {
  currentForecast.html("");
 
   var location = apiSearchData.name;
   var locationTemp = (1.8 * (apiSearchData.main.temp -273) +32).toFixed(2);
   var wind = (apiSearchData.wind.speed).toFixed(2);
-  var humidity = (apiSearchData)
+  var humidity = apiSearchData.main.humidty;
   var icon = apiSearchData.weather[0].icon;
 
   var date = moment().format("M/DD/YYYY");
@@ -64,9 +66,9 @@ var displayWeather = function (apiSearch) {
   var locationEl = $('<h2 style="color:green">');
   var locationTempEl = $('<h4 style="color:green">');
   var windEl = $('<h4 style="color:green">');
-  var humidity = $('<h4 style="color:green">');
-  var iconEl = $('<img>');
-  var urlIcon = `http://openweathermap.org/img/w/${iconCode}.png`;
+  var humidityEl = $('<h4 style="color:green">');
+  var iconElImg = $('<img>');
+  var urlIcon = `http://openweathermap.org/img/w/${icon}.png`;
 
   iconElImg.attr({
     id: "icon",
@@ -88,67 +90,67 @@ var displayWeather = function (apiSearch) {
   currentForecast.append(windEl);
   // humidity
   humidityEl.text(`Humidity: ${humidity}%`);
-  currentForecast.append(windEl);
+  currentForecast.append(humidityEl);
 
 }
 
-var displayWeather = function (apiSearchData) {
-fiveDayForecast.html("");
+// var displayWeather = function (apiSearchData) {
+// fiveDayForecast.html("");
 
-var fiveDayTextDiv = $('<div>')
-fiveDayTextDiv.addClass(`ml-3`);
+// var fiveDayTextDiv = $('<div>')
+// fiveDayTextDiv.addClass(`ml-3`);
 
-var fiveDayTextEl =$('<h3>')
-fiveDayTextEl.text(`Five Day Forecast`);
-fiveDayTextDiv.append(fiveDayTextEl)
-fiveDayForecast.append(fiveDayTextDiv);
+// var fiveDayTextEl =$('<h3>')
+// fiveDayTextEl.text(`Five Day Forecast`);
+// fiveDayTextDiv.append(fiveDayTextEl)
+// fiveDayForecast.append(fiveDayTextDiv);
 
-for (var i = 0; i <= apiSearchData.list.length; i++) {
-  if (i === 4 || i === 12 || i === 20 || i ===28 || i ===36) {
+// for (var i = 0; i <= apiSearchData.list.length; i++) {
+//   if (i === 4 || i === 12 || i === 20 || i ===28 || i ===36) {
 
-    var fiveDayDivEl = $('<div>');
-    fiveDayDivEl.addClass(`columns`)
+//     var fiveDayDivEl = $('<div>');
+//     fiveDayDivEl.addClass(`columns`)
 
-    var dailyForecastEl = $('<div style="border: 3px solid bg-success">');
-    dailyForecastEl.addClass("card m-2 p-1 bg-success text-white")
-    var dateEl = $('<h3>');
-    var locationTempEl = $('<h5>');
-    var humidityEl = $('</h5>');
-    var iconEl = $('<img>');
+//     var dailyForecastEl = $('<div style="border: 3px solid bg-success">');
+//     dailyForecastEl.addClass("card m-2 p-1 bg-success text-white")
+//     var dateEl = $('<h3>');
+//     var locationTempEl = $('<h5>');
+//     var humidityEl = $('</h5>');
+//     var iconEl = $('<img>');
 
-    dateEl.text(moment(apiSearchData.list[i].dt_txt).format("M/DD/YYYY"));
-    fiveDayDivEl.append(dateEl);
+//     dateEl.text(moment(apiSearchData.list[i].dt_txt).format("M/DD/YYYY"));
+//     fiveDayDivEl.append(dateEl);
 
-    var icon = apiSearchData.list[i].weather[0].icon;
-    var urlIcon = `http://openweathermap.org/img/w/${icon}.png`;
+//     var icon = apiSearchData.list[i].weather[0].icon;
+//     var urlIcon = `http://openweathermap.org/img/w/${icon}.png`;
 
-    iconElImg.attr({
-      id: "icon",
-      src: urlIcon,
-      alt: "icon"
-    })
-fiveDayDivEl.append(iconElImg);
+//     iconElImg.attr({
+//       id: "icon",
+//       src: urlIcon,
+//       alt: "icon"
+//     })
+// fiveDayDivEl.append(iconElImg);
 
-var locationTemp = (1.8 * (apiSearchData.main.temp -273) +32).toFixed(2);
-locationTempEl.text(`Temp: ${locationTemp}°F`);
-fiveDayDivEl.append(locationTempEl);
+// var locationTemp = (1.8 * (apiSearchData.main.temp -273) +32).toFixed(2);
+// locationTempEl.text(`Temp: ${locationTemp}°F`);
+// fiveDayDivEl.append(locationTempEl);
 
-var wind = (apiSearchData.wind.speed).toFixed(2);
-windEl.text(`Temp: ${wind} MPH`);
-fiveDayDivEl.append(windEl);
+// var wind = (apiSearchData.wind.speed).toFixed(2);
+// windEl.text(`Temp: ${wind} MPH`);
+// fiveDayDivEl.append(windEl);
 
 
-var humidity = (apiSearchData)
-humidityEl.text(`Humidity: ${humidity}%`);
-fiveDayDivEl.append(humidityEl);
+// var humidity = (apiSearchData)
+// humidityEl.text(`Humidity: ${humidity}%`);
+// fiveDayDivEl.append(humidityEl);
 
-dailyForecastEl.append(fiveDayDivEl);
-fiveDayContainer.append(dailyForecastEl);
+// dailyForecastEl.append(fiveDayDivEl);
+// fiveDayContainer.append(dailyForecastEl);
 
-  }
-}
+//   }
+// }
 
-}
+// }
 
 var renderLocalStorage = function () {
   saveLocation.html("");
@@ -174,87 +176,6 @@ saveLocation.on('click', function(event) {
 renderLocalStorage();
 
 
-// function getWeather() {
-//   // storeWeather();
-//   // console.log('hello');
-//   let city =  textBoxCity.val();
-//   let apiCity =
-//     'https://api.openweathermap.org/data/2.5/weather?q=' +
-//     city +
-//     '&units=imperial&appid=' +
-//     apiKey;
-//   // let lat;
-//   // let lon;
-//   console.log(apiCity);
-//   getWeatherNow(apiCity);
-// }
-
-// function getWeatherNow(apiCity) {
-//   fetch(apiCity)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     cityLocation.text(`${data.name}`);
-//     temp.text(
-//       `${Math.trunc(data.main.temp)} ${String.fromCharCode(176)} °F`
-//     );
-//     wind.text(`Wind: ${data.wind.speed} MPH`);
-//     humidity.text(`Humidity: ${data.main.humidity} %`)
-//     let lat = data.coord.lat;
-//     let lon = data.coord.lon;
-//     currentForecast(lat, lon);
-
-//   });
-// }
-
-// function forecast(lat, lon) {
-//   let currentForecastApi = 
-//   'https://api.openweathermap.org/data/2.5/forecast?lat=' +
-//     lat +
-//    '&lon=' +
-//     lon +
-// '&units=imperial&appid=' +
-// apiKey;
-
-// fiveDayForecast(currentForecastApi);
-// }
-
-// function fiveDayForecast(currentForecastApi) {
-//   fetch(currentForecastApi)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data){
-//     console.log(data)
-//     let x=0;
-//     for (let i =3; i < 36; i += 8) {
-//       let time = data.list[i].dt_txt.split(" ");
-
-//       document.querySelector(`#day${x}`).children[0].textContent =
-//       time[0];
-//       document.querySelector(
-//         `#day-${x}`
-//       ).children[1].textContent = `Temp: ${Math.trunc(
-//         data.list[i].main.temp
-//       )} ${String.fromCharCode(176)} °F`;
-//       document.querySelector(
-//         `day-${x}`
-//       ).children[2].textContent = `Wind: ${data.list[i].wind.speed} MPH`;
-//       document.querySelector(
-//         `day-${x}`
-//       ).children[3].textContent = `Humidity: ${data.list[i].main.humidity} %`;
-//       x++; 
-//     }
-//   });
-// }
-// citySearchForm.on("submit", function (e) {
-//   e.preventDefault();
-//   getWeatherNow();
-//   textBoxCity.val("");
-
-// });
 
 
 
